d55b868028956dbab01d7bf9c3b565b2b0883a4d
Sulenias Asso <74882893+niasasso@users.noreply.github.com>
2022-07-25 21:56:26 +0900

    tugas day01 project01

---
+++ pengulangan.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Belajar PHP</title>
+</head>
+<body>
+
+	<h1>Loop PHP</h1>
+
+	<?php 
+
+		// ------ Pengulangan -----
+		// for(variabelawal(mulai); batas(syarat); perubahan)
+
+		for($i = 0; $i < 5; $i++) 
+		{
+			echo "---------------";
+			echo "Sekolah Coding";
+			echo "--------------- <br>";
+		}
+	 ?>
+
+</body>
+</html>
